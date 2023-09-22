b409071de17fde519890cdd7aab6101e62bb3ece
Anupam Tripathi <anupamtripathi1433@gmail.com>
2023-09-22 18:38:18 +0530

    Adding dataview project

---
+++ dataview/load_data.py
@@ -0,0 +1,34 @@
+from myapp.models import Company
+import os
+import csv
+from django.conf import settings
+# from myapp.models import Company
+
+
+def load_data():
+    csv_file_path = os.path.join(
+        settings.BASE_DIR, 'C:/Users/anupa/Desktop/Consolidation/Main_dataset.csv')
+
+    with open(csv_file_path, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the header row if it exists
+
+        for row in reader:
+            company = Company(
+                company_name=row[0],
+                category=row[1],
+                sector=row[2],
+                share_price=float(row[3]),
+                market_cap=float(row[4]),
+                source=row[5],
+                lot_size=int(row[6]),
+                min_qty=int(row[7]),
+                isin=row[8],
+                special_comment=row[9],
+                sell_price_rupees=int(row[10])
+            )
+            company.save()
+
+
+if __name__ == "__main__":
+    load_data()
---
+++ dataview/myapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
+    sector = models.CharField(max_length=100)
+    share_price = models.DecimalField(max_digits=10, decimal_places=2)
+    market_cap = models.DecimalField(max_digits=15, decimal_places=2)
+    source = models.URLField(blank=True)  # Assumes URLs for the source
+    lot_size = models.PositiveIntegerField()
+    min_qty = models.PositiveIntegerField()
+    isin = models.CharField(max_length=20)
+    special_comment = models.TextField(blank=True)
+    sell_price_rupees = models.PositiveIntegerField(blank=True, null=True)
